eb578240aaf3d819d697230889d3c252833c7c77
JP Gutton <jpgutton@microsoft.com>
2021-03-01 12:56:28 -0500

    Add project files.

---
+++ WebFE/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebFE.Models;
+
+namespace WebFE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+        public async Task<IActionResult> API()
+        {
+            List<WeatherForecast> weatherList = new List<WeatherForecast>();
+
+            using (var client = new HttpClient())
+            {
+                var uri = new Uri("http://localhost:5001/WeatherForecast");
+
+                var response = client.GetAsync(uri).Result;
+
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception(response.ToString());
+
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                weatherList = JsonConvert.DeserializeObject<List<WeatherForecast>>(apiResponse);
+            }
+
+            return View(weatherList);
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
